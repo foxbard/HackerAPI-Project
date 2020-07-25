7abef10ab1274cb41f2715eda65d9525c4b7eaa7
jrfoxw <jrfoxw@gmail.com>
2020-07-25 11:03:54 -0500

    Commited Web API Unit Tests

---
+++ APIClient/HackerApiClientTests/APIIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerAPI.Client.Integration.Test
+{
+    public class APIIntegrationTest
+    {
+        [Fact]
+        public async Task Test_GET()
+        {
+            using var client = new TestClientProviderA().Client;
+            var response = await client.GetAsync("/api");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK); 
+
+        }
+    }
+}
---
+++ APIClient/HackerApiClientTests/TestClientProviderA.cs
@@ -0,0 +1,28 @@
+﻿using hackerAPIClient;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HackerAPI.Client.Integration.Test
+{
+    public class TestClientProviderA : IDisposable
+    {
+        private readonly TestServer nServer;
+        public HttpClient Client { get; private set; }
+        public TestClientProviderA()
+        {
+            nServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = nServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            //dispose of test server/client
+            nServer?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
