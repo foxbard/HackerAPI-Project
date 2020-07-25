6148e3bed32f8f86a276721833b4ae930c7a3bac
jrfoxw <jrfoxw@gmail.com>
2020-07-25 12:09:59 -0500

    Web Api Commit

---
+++ APIClient/hackerAPIServer/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace hackerAPI.Client.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult api()
+        {
+            return Ok("Api online");
+        }
+    }
+}
